0db7d0ac8fb76d0d6d0070027ce18cc847241601
Marcos <mtg200416@gmail.com>
2026-04-06 18:58:59 +0200

    subiendo el pryecto hecho completo

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -5,6 +5,7 @@ SELECT * FROM species;
 SELECT * FROM climate;
 SELECT * FROM observations;
 
+#nivel 1
 select * from observations limit 10;
 
 select DISTINCT id from regions;
@@ -18,7 +19,7 @@ select   count(observation_date) from observations where observation_date = '199
 
 
 
-
+#nivel 2
 select region_id,count(*) as observaciones from observations group by region_id order by observaciones desc ;
 
 select species_id,count(*) as especies from observations group by species_id order by especies desc limit 5 ;
@@ -29,7 +30,7 @@ select observer ,count(*) as observaciones from observations group by observer o
 
 
 
-
+#nivel 3
 SELECT observations.id, regions.name AS nombre_region, observations.observation_date from observations JOIN regions ON observations.region_id = regions.id;
 
 SELECT species.id, species.scientific_name AS nombre_especie from observations JOIN species ON observations.species_id = species.id;
@@ -37,6 +38,8 @@ SELECT species.id, species.scientific_name AS nombre_especie from observations J
 select regions.name as region,species.scientific_name,count (*) as total from observations join species on observations.species_id = species.id join regions on observations.region_id = regions.id group by region,species.scientific_name order by region,total desc;
 
 
+
+#opcionales
 INSERT INTO observations (species_id,region_id,observer,observation_date,count) values (6 , 3, 'tecnico de sistemas','2027-04-05',5);
 
 UPDATE species set scientific_name = 'Enicognathus ferrugineuss' where scientific_name = 'Enicognathus ferrugineus';
